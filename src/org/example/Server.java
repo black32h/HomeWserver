54ff87949c890299ebc5cc8dad98670c7d4a9035
black32h <blackspetya@gmail.com>
2024-11-19 19:51:37 +0200

    Update Server.java

--- src/org/example/Server.java
+++ src/org/example/Server.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main ( String[] args ) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package org.example;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static final int PORT = 5000;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущено на порту: " + PORT);
+
+            while (true) {
+                System.out.println("Очікування підключення клієнта...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Клієнт підключився: " + clientSocket);
+
+                try (DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                     DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())) {
+
+                    // Отримання метаданих файлу
+                    String fileName = in.readUTF();
+                    int fileSize = in.readInt();
+
+                    System.out.println("Отримано файл: " + fileName + " (Розмір: " + fileSize + " байт)");
+
+                    if (fileSize <= 1024) {
+                        // Отримання файлу
+                        byte[] fileData = new byte[fileSize];
+                        in.readFully(fileData);
+
+                        // Збереження файлу на сервері
+                        File file = new File(fileName);
+                        try (FileOutputStream fos = new FileOutputStream(file)) {
+                            fos.write(fileData);
+                            System.out.println("Файл збережено на сервері: " + file.getAbsolutePath());
+                        }
+
+                        // Відправлення файлу назад клієнту
+                        out.writeUTF("Файл успішно отримано та збережено.");
+                        out.writeInt(fileSize);
+                        out.write(fileData);
+                        System.out.println("Файл відправлено назад клієнту.");
+                    } else {
+                        out.writeUTF("Файл занадто великий. Повинен бути ≤ 1 КБ.");
+                        System.out.println("Файл не задовольняє умовам.");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Помилка під час обробки файлу: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Помилка запуску сервера: " + e.getMessage());
         }
     }
 }
